0b44ac7fb46b3f369c6dc763a5e8c5082863bd2f
Sergey Alipchenkov <s.alipchenkov@assistagro.com>
2022-08-05 22:14:16 +0300

    Download file by ID

--- cient.go
+++ cient.go
@@ -1,6 +1,8 @@
 package sentinel
 
 import (
+	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -28,4 +30,35 @@ func NewClient() *SentinelClient {
 		searchURL:  "https://scihub.copernicus.eu/dhus/search?q=",
 		rows:       100,
 	}
+}
+
+func (c *SentinelClient) Download(id string, dst string) error {
+	fmt.Println("GOT ID: ", id)
+	link := fmt.Sprintf("https://scihub.copernicus.eu/dhus/odata/v1/Products('%s')/$value", id)
+
+	fmt.Printf("Downloading file %s to %s\n", link, dst)
+	out, err := os.Create(dst)
+	if err != nil {
+		return fmt.Errorf("error on create local index file: %s", err)
+	}
+	defer out.Close()
+
+	req, err := http.NewRequest("GET", link, nil)
+	if err != nil {
+		return fmt.Errorf("error on create request: %s", err)
+	}
+	req.SetBasicAuth(c.user, c.password)
+
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("error on get file: %s", err)
+	}
+	defer resp.Body.Close()
+
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		return fmt.Errorf("error on saving file: %s", err)
+	}
+	fmt.Println("Download ready")
+	return nil
 }
--- examples/search.go
+++ examples/search.go
@@ -14,6 +14,7 @@ func main() {
 	tiles := []string{"36UYA", "36UYB", "36UYC", "36UYD", "36UYE", "37TDK", "37TDL", "37TEL", "37TEM", "37UCR", "37UCS", "37UCT", "37UCU", "37UCV", "37UDR", "37UDS", "37UDT", "37UDU", "37UDV", "37UEU", "37UEV", "37UFT", "37UFU", "37UFV", "37UGT", "37UGU", "38ULD", "44UPG", "45UUB"}
 
 	resCount := 0
+	entries := make([]sentinel.QueryEntryResponse, 0)
 	for _, tile := range tiles {
 		searchParameters := sentinel.SearchParameters{
 			Platforms: []sentinel.Platform{sentinel.PlanformSentinel2},
@@ -21,7 +22,7 @@ func main() {
 			TileIDs: []string{tile},
 			// ProductTypes: []string{"S2MSI2A", "S2MS2Ap"},
 			// ProductTypes:  []string{"S2MSI1C"},
-			BeginPosition: "[2021-01-01T00:00:00.000Z TO NOW]",
+			BeginPosition: "[2022-01-01T00:00:00.000Z TO NOW]",
 		}
 
 		res, err := client.Query(searchParameters)
@@ -29,7 +30,17 @@ func main() {
 			log.Fatal(err)
 		}
 		resCount += res.Feed.TotalResults
+		entries = append(entries, res.Feed.Entries...)
 	}
 	fmt.Printf("Total found %d items\n", resCount)
+
+	if resCount > 0 {
+		// fmt.Printf("%+v\n", entries[0])
+		// fmt.Printf("%+v\n", string(entries[0].Date))
+		client.Download(entries[0].ID, "/tmp/test.zip")
+		// fmt.Printf("%+v\n", entries[0].Link[0])
+		// tools.DownloadFile(entries[0].Link[0].HREF, "/tmp/1.zip")
+
+	}
 
 }
